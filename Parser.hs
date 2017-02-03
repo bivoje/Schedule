81e66786478c5f1399d47cb6adf76fb15a7a1d22
bivoje <bivoje49@gmail.com>
2017-02-03 21:42:35 +0900

    Create timetable parser

--- Parser.hs
+++ Parser.hs
@@ -1,10 +1,10 @@
 {-# LANGUAGE NoMonomorphismRestriction #-}
 {-# LANGUAGE FlexibleContexts #-}
 
-module Parser (parse_openlects,word) where
+module Parser (parse_openlects,parse_timetable,word) where
 
 import Data.Char (isSpace)
-import Data.List (intercalate)
+import Data.List
 import Text.Parsec
 import Text.Parsec.Token
 
@@ -25,28 +25,27 @@ import Text.Parsec.Token
 
 
 -- FIXME readFile might produce an exception
-parse_openlects file = readFile file >>= return . parse table file
+parse_openlects file = readFile file >>= return . parse opl_table file
 
 -- read whole table, returns its content
-table = do
-  hd <- header
-  tl <- title
-  ct <- content (length tl)
+opl_table = do
+  hd <- opl_header
+  tl <- opl_title
+  ct <- opl_content (length tl)
   return $ hd : tl : ct
 
 -- read heading of table (name of table etc..) returns ()
-header = count 2 $ cell >>= followedBy (many tab >> newline) >> return []
+opl_header = count 2 line >> return []
 
 -- read titles of column, returns number of columns
-title = cell `sepBy` tab
-        >>= followedBy newline
+opl_title = cells
 
 -- read content of a table, ignore worthless footer
-content num = fmap concat $ many1 (school num)
+opl_content num = fmap concat $ many1 (school num)
 
 -- read a category of school like (기초교육학부)
 school num = do
-  lss <- many1 (row num) >>= followedBy (try newline >> eof)
+  lss <- many1 (opl_row num) >>= followedBy (try newline >> eof)
   let l = head lss
       scl = head l
       ss = tail lss
@@ -54,16 +53,81 @@ school num = do
  
 -- :: [String]
 -- read a row, any data in openlects is consist of a row
-row num = do
+opl_row num = do
   x <- count (num-1) (cell >>= followedBy tab)
   y <- cell >>= followedBy newline
   return $ x ++ [y]
-  <?> "row"
+ <?> "opl_row"
+
+
+-------------------------------------------------------------------
+-- Timetable
+
+
+-- top-most utility function
+parse_timetable file = readFile file >>= return . parse tmt_table file
+
+-- read whole table, returns its content
+tmt_table = do
+  tl <- tmt_title
+  ct <- tmt_content (tail $ map snd tl)
+  return ct
+
+-- read title of each column,
+-- used to calculate how many cell are in each day
+tmt_title = many1 mergedCell >>= followedBy newline
+            <?> "title"
+
+-- read content of table
+tmt_content ncells = do
+  morning    <- count 2 tp
+  line --lunch time
+  afternoon  <- count 5 tp
+  many emptyline
+  experiment <- count 2 tp
+  many emptyline
+  activity   <- count 3 tp
+  return (morning ++ afternoon ++ experiment ++ activity)
+ <?> "content"
+  where tp = timep ncells
+
+-- read single time period
+-- cell of beginig is for timep name e.g. "1st Class (09:00-10:15)"
+timep ncells = do
+  cell
+  cs <- many1 tmt_row
+  return $ daize cs ncells
+ <?> "timep"
+  where daize _ [] = []
+        daize rows (n:ncs) =
+          let sp = map (splitAt n) rows
+           in concatMap fst sp : daize (map snd sp) ncs
+
+-- read a row of vertical blocks consists of
+-- [Crs. No., Instructor, Class, Title Room]
+-- then remove labels, group the block by each
+tmt_row = do
+  a <- try $ count 4 (tab >> cells)
+  return . map tuplize . tail . transpose $ a
+ <?> "tmt_row"
+  where tuplize [a,b,c,d] = (a,b,c,d)
 
 
 -------------------------------------------------------------------
 -- Primary
 
+
+-- read a horizontally merged cell
+-- returns the number of cells merged with the cell content
+mergedCell = do
+  c <- cell
+  t <- many1 tab
+  return (c, length t)
+ <?> "mergedCell"
+
+-- returns a line of cells
+cells = cell `sepBy` tab >>= followedBy newline
+        <?> "cells"
 
 -- ::[Char]
 -- read each cell of a row
@@ -72,6 +136,16 @@ cell =
   try (between (char '"') (char '"') (except '"'))
   <|> many (noneOf "\t\n")
   <?> "cell"
+
+
+-- skip an empty line (filled with tabs)
+-- quoted \n character not supported
+emptyline = skipMany tab >> newline >> return ()
+            <?> "emptyline"
+
+-- skips a line
+line = except '\n' >>= followedBy newline
+       <?> "line"
 
 followedBy :: Stream s m Char
            => ParsecT s u m t -> a -> ParsecT s u m a
