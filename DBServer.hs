2d7ef736b44f36a1db42717789f0f310a5ee25f9
bivoje <bivoje@gmail.com>
2017-02-16 21:14:08 +0900

    Create DBServer module
    this module will do all the works connecting to server

---
+++ DBServer.hs
@@ -0,0 +1,7 @@
+module DBServer where
+
+import Types.Internal
+{- DBServer module imports Types.Internal directly
+ - and provides getter-functions of some composite types.
+ - Using those functions would guarantee the validity of the data.
+ -}
