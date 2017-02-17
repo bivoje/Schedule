af0dd15870faf25373a730df250e6b797f80dc66
bivoje <bivoje@gmail.com>
2017-02-17 15:44:59 +0900

    Extend interface of Types module

--- Types.hs
+++ Types.hs
@@ -1,21 +1,47 @@
 
-module Types
-  ( Season(..)
+module Types (
+
+  {- NOTION of constructor hiding
+   - there are some exporting their constructors
+   - because they can verify their validity
+   - with its own constructors only
+   - others are not e.g. Crsid (GS,999999) is not valid but possible
+   - and does not export there constructors
+   -}
+
+  -- basic types (fields)
+    Season(..)
+
   , WeekDay(..)
+  ,   weekdayTstring
+  ,   stringTweekday
+
   , Semester()
+  ,   semesterTint
+  ,   intTsemester
+
   , School(..)
+  ,   schoolTstring
+  ,   stringTschool
+
   , Crsid()
+  ,   crsidTstring
+  ,   stringTcrsid
+
   , Lectime()
+  , LecTime()
+
   , RoomId() -- for future safety
-  , Prof(..)
-  , Course(..)
-  , Section(..)
-  , RefCrs(..)
-  , RefSect(..)
-  , schoolTstring
-  , stringTschool
-  , crsidTstring
-  , stringTcrsid
+
+  -- db table types
+  , Prof()
+  , TeachAssi()
+  , Course()
+  , Section()
+
+  , RefCrs()
+  , RefSect()
+
   ) where
 
 import Types.Internal
