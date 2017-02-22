c7dd1118d36e18cb8e8c41ec4012753addfa4282
bivoje <bivoje@gmail.com>
2017-02-23 03:31:33 +0900

    Add routine making image of elem course-checklist

--- Schedule/ConsoleIO.hs
+++ Schedule/ConsoleIO.hs
@@ -36,6 +36,28 @@ yet = Nothing
 type Check a = (a, TriStat)
 
 
+{-
+we could return string to print in list, so that caller
+can convert it to image, or do whatever he wants.
+(c.f. chrsString :: Check Course -> String)
+But, it turned out to be  really hard to cut a string
+in title_kr with fixed garaphical width, since full- and
+half- width characters are mixed in it.
+Vty library functions (crop-seriese) support this like a charm!
+-}
+-- create Image for check Course to be used in checking list
+chcrsImage :: Attr -> Check Course -> Image
+chcrsImage attr (crs,s) =
+  let cid = text attr $ crsidTstr (crs_id crs)
+      tsp = charFill attr ' ' 50 1
+      tlt'= text' attr $ title_kr crs
+      tlt = cropRight 50 $ tlt' <|> tsp
+      cre = string attr $ show (credit crs)
+      gab = char attr ' '
+      tet = intersperse gab [cid,cre,tlt]
+    in mkcheckImage attr s $ horizCat tet
+
+
 -- TODO we might have to make typeclass for this
 -- (converting to image, ToImage, like ToJSON)
 
