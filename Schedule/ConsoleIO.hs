f2a2bebd8ff9c1c0985360e64aaee29decfce21d
bivoje <bivoje@gmail.com>
2017-02-23 03:14:06 +0900

    Define Check and Tristat type synonyms

--- Schedule/ConsoleIO.hs
+++ Schedule/ConsoleIO.hs
@@ -11,3 +11,26 @@ import Control.Monad.RWS
 
 import Schedule.Types
 import Schedule.Zipper
+
+
+type TriStat = Maybe Bool
+
+{-
+pattern matching to contant is always against
+(1) integers or (2) Capital lettered (contructor)
+a variable (normal function) cannot be used in
+pattern matching, so be careful, do not accidently
+use these functions in pattern matching.
+use the direct constructors instead (e.g. no -> Just True)
+-}
+
+yes :: TriStat
+yes = Just True
+
+no :: TriStat
+no = Just False
+
+yet :: TriStat
+yet = Nothing
+
+type Check a = (a, TriStat)
