c0efc6c5e3cf2b238079e1847604f8ac7ce37f35
bivoje <bivoje@gmail.com>
2017-03-02 09:26:35 +0900

    Create timetable rendering routine

--- Schedule/ConsoleIO.hs
+++ Schedule/ConsoleIO.hs
@@ -1,9 +1,13 @@
 module Schedule.ConsoleIO where
 
+import Data.List (transpose)
+import Data.Text (Text)
+import qualified Data.Text as T
 import Control.Monad.RWS
 
 import Graphics.Vty
 
+import Schedule.Types
 import Schedule.Types.Zipper
 import Schedule.Types.Console
 
@@ -42,3 +46,24 @@ viewCheckZipper :: ToImage a => Pick a ()
 viewCheckZipper = do
    pic <- gets $ picForImage . imagine defAttr . ZipperW . (,) 10
    ask >>= liftIO . flip update pic
+
+
+renderTimetable = renderTimetable' ('│','─','┼',' ',8,3)
+
+-- assumes ttm is proper matrix
+renderTimetable' :: (Char,Char,Char,Char,Int,Int)
+                 -> [[Maybe RefSect]] -> Text
+renderTimetable' (horzd,vertd,cross,sps,celw,nu) ttm' =
+  let heading' = map (T.center celw sps . weekdayTstr) [Monday .. Friday]
+      heading = rep nu sps `T.append` rowlize horzd heading'
+      ttm = transpose ttm'
+      content = map (rowlize horzd . map rRefSect) ttm
+      horline' = replicate 5 $ T.replicate celw $ T.singleton vertd
+      horline = rep nu vertd `T.append` rowlize cross horline'
+      label = map (T.justifyRight nu sps . T.pack . show) [1..]
+   in T.unlines $ heading : horline : zipWith mappend label content
+  where rep n = T.replicate n . T.singleton
+        rRefSect Nothing = T.replicate celw (T.singleton sps)
+        rRefSect (Just x) = refsectTstr x
+        rowlize d ls =
+          horzd `T.cons` T.intercalate (T.singleton d) ls `T.snoc` horzd
