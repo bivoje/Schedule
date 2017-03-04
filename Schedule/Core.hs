61fb2d6d404d293eead5d913282a92be149aa419
bivoje <bivoje@gmail.com>
2017-03-04 21:45:47 +0900

    Add relation violoation detect routine

--- Schedule/Core.hs
+++ Schedule/Core.hs
@@ -5,3 +5,16 @@ import Control.Monad
 
 import Schedule.Types
 import Schedule.DBServer
+import qualified Data.Set as S
+
+
+requirConflicts :: CrsidSet -> [Crsid] -> DB [(Crsid, CrsidSet)]
+requirConflicts took crss =
+  filter (null . S.intersection took . snd) . zip crss
+    <$> mapM getRequir crss
+
+
+retakeConfilits :: CrsidSet -> [Crsid] -> DB [(Crsid, CrsidSet)]
+retakeConfilits took crss =
+  filter (not . null . S.intersection took . snd) . zip crss
+    <$> mapM getRetake crss
